30c64ccbd0e407a88c827dbe21a2b99c2c6a3761
arfenis <arfen00@gmail.com>
2022-02-14 15:01:30 -0400

    Create pipelines methods and tests

--- src/Pipelines/Pipelines.php
+++ src/Pipelines/Pipelines.php
@@ -11,15 +11,98 @@ use Phalcon\Utils\Slug;
 
 class Pipelines
 {
-    public static function create(string $name, BaseModel $entity)
+    /**
+     * Create a new pipeline based on name and entity
+     *
+     * @param string $name
+     * @param BaseModel $entity
+     * @return ModelsPipelines
+     */
+    public static function create(string $name, BaseModel $entity) : ModelsPipelines
     {
         $pipeline = new ModelsPipelines();
         $pipeline->entity_namespace = get_class($entity);
         $pipeline->name = $name;
         $pipeline->users_id = Di::getDefault()->get('userData')->getId();
+        $pipeline->companies_id = Di::getDefault()->get('userData')->companies_id;
         $pipeline->slug = Slug::generate($name);
         $pipeline->saveOrFail();
 
         return $pipeline;
     }
+
+    /**
+     * Get all pipelines associated to a company
+     *
+     * @param integer $page
+     * @param integer $limit
+     * @return array
+     */
+    public static function getAll($page = 1, $limit = 10) : array
+    {
+        $offset = ($page - 1) * $limit;
+
+        $pipelines = ModelsPipelines::find([
+            'conditions' => 'companies_id = :company_id: AND is_deleted = 0',
+            'bind' => [
+                'company_id' => Di::getDefault()->get('userData')->companies_id
+            ],
+            'limit' => $limit,
+            'offset' => $offset
+        ]);
+
+        return $pipelines->toArray();
+    }
+
+
+    /**
+     * Get a pipeline by its id
+     *
+     * @param integer $id
+     * @return ModelsPipelines
+     */
+    public static function getById(int $id) : ModelsPipelines
+    {
+        return ModelsPipelines::findFirstOrFail(
+            [
+                'conditions' => 'id = :id: AND companies_id = :companies_id: AND is_deleted = 0',
+                'bind' => [
+                    'id' => $id,
+                    'companies_id' => Di::getDefault()->get('userData')->companies_id,
+                ]
+            ]
+        );
+    }
+
+
+    /**
+     * Update pipeline
+     *
+     * @param ModelsPipelines $pipeline
+     * @param string $name
+     * @return ModelsPipelines
+     */
+    public static function update(ModelsPipelines $pipeline, string $name) : ModelsPipelines
+    {
+        $pipeline->name = $name;
+        $pipeline->saveOrFail();
+
+        return $pipeline;
+    }
+
+    // public static function findByRawSql(int $page = 1, int $limit = 10, $params = null)
+    // {
+    //     $page = ($page - 1) * $limit;
+    //     $sql = "SELECT 
+    //             e.*
+    //             FROM pipelines as w , notable_widgets_entities as e 
+    //                 WHERE  
+    //                     w.widget_uuid  = '$uuid'
+    //                     AND e.notable_widgets_id = w.id
+    //                     AND e.is_deleted = 0
+    //                 ORDER BY e.order
+    //                 LIMIT $page, $limit";
+
+    //     return ModelsPipelines::findByRawSql($sql);
+    // }
 }
--- tests/integration/Pipelines/PipelinesCest.php
+++ tests/integration/Pipelines/PipelinesCest.php
@@ -5,17 +5,68 @@ declare(strict_types=1);
 namespace Kanvas\Guild\Tests\Integration\Pipelines;
 
 use IntegrationTester;
+use Kanvas\Guild\Pipelines\Models\Pipelines as ModelsPipelines;
 use Kanvas\Guild\Pipelines\Pipelines;
 use Kanvas\Guild\Tests\Support\Models\Missions;
 
 class PipelinesCest
 {
+    public ModelsPipelines $pipeline;
+
+    /**
+     * Test create a new pipeline
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
     public function testCreatePipeline(IntegrationTester $I) : void
     {
         $name = "Thinking";
 
         $pipeline = Pipelines::create($name, new Missions());
 
+        $this->pipeline = $pipeline;
+
         $I->assertEquals($name, $pipeline->name);
+    }
+
+    /**
+     * Get all pipelines
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
+    public function testGetAllPipelines(IntegrationTester $I) : void
+    {
+        $pipelines = Pipelines::getAll();
+
+        $I->assertTrue(isset($pipelines[0]['id']));
+    }
+
+    /**
+     * Test pipeline by id
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
+    public function testGetPipelineById(IntegrationTester $I) : void
+    {
+        $pipeline = Pipelines::getById($this->pipeline->getId());
+
+        $I->assertEquals($pipeline->getId(), $this->pipeline->getId());
+    }
+
+
+    /**
+     * Update pipeline
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
+    public function testUpdatePipeline(IntegrationTester $I) : void
+    {
+        $pipeline = Pipelines::update($this->pipeline, "Update Pipeline");
+
+        $I->assertEquals($pipeline->name, $this->pipeline->name);
     }
 }
