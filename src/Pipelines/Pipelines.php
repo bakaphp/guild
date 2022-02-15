8532c4054bd5da19b39ba3f22921507f61551f29
arfenis <arfen00@gmail.com>
2022-02-15 17:45:33 -0400

    Create methods to manage pipelines tages

--- src/Pipelines/Pipelines.php
+++ src/Pipelines/Pipelines.php
@@ -7,7 +7,7 @@ namespace Kanvas\Guild\Pipelines;
 use Kanvas\Guild\BaseModel;
 use Kanvas\Guild\Contracts\UserInterface;
 use Kanvas\Guild\Pipelines\Models\Pipelines as ModelsPipelines;
-use Phalcon\Di;
+use Kanvas\Guild\Pipelines\Models\PipelinesStages;
 use Phalcon\Utils\Slug;
 
 class Pipelines
@@ -62,14 +62,14 @@ class Pipelines
      * @param integer $id
      * @return ModelsPipelines
      */
-    public static function getById(int $id) : ModelsPipelines
+    public static function getById(int $id, UserInterface $user) : ModelsPipelines
     {
         return ModelsPipelines::findFirstOrFail(
             [
                 'conditions' => 'id = :id: AND companies_id = :companies_id: AND is_deleted = 0',
                 'bind' => [
                     'id' => $id,
-                    'companies_id' => Di::getDefault()->get('userData')->companies_id,
+                    'companies_id' => $user->getCompanies(),
                 ]
             ]
         );
@@ -89,5 +89,93 @@ class Pipelines
         $pipeline->saveOrFail();
 
         return $pipeline;
+    }
+
+    /**
+     * Create a new pipeline stage
+     *
+     * @param ModelsPipelines $pipeline
+     * @param string $name
+     * @param boolean $hasRotting
+     * @param integer $rottingDays
+     * @return PipelinesStages
+     */
+    public static function createStage(ModelsPipelines $pipeline, string $name, bool $hasRotting = false, int $rottingDays = 0) : PipelinesStages
+    {
+        $pipelineStage = new PipelinesStages();
+        $pipelineStage->name = $name;
+        $pipelineStage->pipelines_id = $pipeline->getId();
+        $pipelineStage->has_rotting_days = $hasRotting;
+        $pipelineStage->rotting_days = $rottingDays;
+        $pipelineStage->saveOrFail();
+        
+        return $pipelineStage;
+    }
+
+    /**
+     * Update a pipeline stage
+     *
+     * @param PipelinesStages $stage
+     * @param array $data
+     * @return PipelinesStages
+     */
+    public static function updateStage(PipelinesStages $stage, array $data) : PipelinesStages
+    {
+        $updateFields = [
+            'name',
+            'title',
+            'has_rotting_days',
+            'rotting_days',
+            'weight'
+        ];
+
+        $stage->saveOrFail($data, $updateFields);
+
+        return $stage;
+    }
+
+
+    /**
+     * Get Pipeline Stage by id
+     *
+     * @param UserInterface $user
+     * @param integer $id
+     * @return PipelinesStages
+     */
+    public static function getStageById(int $id) : PipelinesStages
+    {
+        return PipelinesStages::findFirstOrFail(
+            [
+                'conditions' => 'id = :id: AND is_deleted = 0',
+                'bind' => [
+                    'id' => $id,
+                ]
+            ]
+        );
+    }
+
+
+    /**
+     * Get all the stages from a pipeline
+     *
+     * @param ModelsPipelines $pipeline
+     * @param integer $page
+     * @param integer $limit
+     * @return array
+     */
+    public static function getStagesByPipeline(ModelsPipelines $pipeline, int $page = 1, int $limit = 10) : array
+    {
+        $offset = ($page - 1) * $limit;
+
+        $pipelines = PipelinesStages::find([
+            'conditions' => 'pipelines_id = :pipelines_id: AND is_deleted = 0',
+            'bind' => [
+                'pipelines_id' => $pipeline->getId()
+            ],
+            'limit' => $limit,
+            'offset' => $offset
+        ]);
+
+        return $pipelines->toArray();
     }
 }
