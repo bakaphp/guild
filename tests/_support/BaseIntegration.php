b3ebe19d82eb7b4f0ddeafb199e4b192cd4443dd
arfenis <arfen00@gmail.com>
2022-03-08 18:10:57 -0400

    Create a base intergration test files

---
+++ tests/_support/BaseIntegration.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Guild\Tests\Support;
+
+use Helper\DataBuilder;
+use Kanvas\Guild\Tests\Support\Models\Users;
+
+class BaseIntegration
+{
+    public DataBuilder $dataBuilder;
+
+    public function onConstruct()
+    {
+        $this->dataBuilder = new DataBuilder(new Users());
+    }
+}
