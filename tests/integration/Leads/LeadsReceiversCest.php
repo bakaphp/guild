5a86c46ae462b2359c7b6da18d9c8752577b83f3
arfenis <arfen00@gmail.com>
2022-03-11 11:13:00 -0400

    Add receiver to agent creation

--- tests/integration/Leads/LeadsReceiversCest.php
+++ tests/integration/Leads/LeadsReceiversCest.php
@@ -5,15 +5,14 @@ declare(strict_types=1);
 namespace Kanvas\Guild\Tests\Integration\Pipelines;
 
 use IntegrationTester;
-use Kanvas\Guild\Rotations\Models\Rotations;
 use Kanvas\Guild\Leads\Models\Receivers as ModelsReceivers;
 use Kanvas\Guild\Leads\Receivers;
+use Kanvas\Guild\Tests\Support\BaseIntegration;
 use Kanvas\Guild\Tests\Support\Models\Users;
 
-class LeadsReceiversCest
+class LeadsReceiversCest extends BaseIntegration
 {
     public ModelsReceivers $receiver;
-
 
     /**
      * Test create of a new Receiver
@@ -26,7 +25,7 @@ class LeadsReceiversCest
         $receiver = Receivers::create(
             new Users(),
             'Receiver in Testing',
-            Rotations::findFirst(),
+            $this->dataBuilder->createRotation(),
             'Source name development',
             true
         );
@@ -45,19 +44,15 @@ class LeadsReceiversCest
      */
     public function testUpdateLeadReceiver(IntegrationTester $I) : void
     {
-        $rotation = Rotations::findFirst([
-            'conditions' => 'id != :old_rotation: AND is_deleted = 0',
-            'bind' => [
-                'old_rotation' => $this->receiver->rotations_id
-            ]
-        ]);
+        $rotation = $this->dataBuilder->createRotation();
+        $receiver = $this->dataBuilder->createReceiver();
 
         $data = [
             'name' => 'Receiver updated name',
             'source_name' => 'New source name'
         ];
 
-        $updateReceiver = Receivers::update($this->receiver, $data, $rotation);
+        $updateReceiver = Receivers::update($receiver, $data, $rotation);
         
         $I->assertEquals($data['name'], $updateReceiver->name);
         $I->assertEquals($rotation->getId(), $updateReceiver->rotations_id);
