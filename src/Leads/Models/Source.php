4501940276254f17aa25117812979a6c0d022fa1
arfenis <arfen00@gmail.com>
2022-03-11 11:15:57 -0400

    Create models for leads and direct relateds

---
+++ src/Leads/Models/Source.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Guild\Leads\Models;
+
+use Kanvas\Guild\BaseModel;
+
+class Source extends BaseModel
+{
+    public ?int $apps_id = 0;
+    public int $companies_id;
+    public string $name;
+    public ?string $description = null;
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->setSource('leads_sources');
+
+        $this->hasMany(
+            'leads_sources_id',
+            Leads::class,
+            'id',
+            [
+                'reusable' => true,
+                'alias' => 'leads',
+            ]
+        );
+    }
+}
