37b1a64aa570ad1435c68ebad6e06fae5f5262e0
arfenis <arfen00@gmail.com>
2022-03-18 12:11:18 -0400

    Update activities migrations

---
+++ storage/db/migrations/20220318161049_update_migrations_activities.php
@@ -0,0 +1,51 @@
+<?php
+
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class UpdateMigrationsActivities extends Phinx\Migration\AbstractMigration
+{
+    public function change()
+    {
+        $this->table('activities_types', [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_520_nopad_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->changeColumn('name', 'string', [
+                'null' => false,
+                'limit' => 150,
+                'collation' => 'utf8mb4_unicode_520_nopad_ci',
+                'encoding' => 'utf8mb4',
+                'after' => 'companies_id',
+            ])
+            ->changeColumn('description', 'text', [
+                'null' => true,
+                'default' => null,
+                'limit' => 65535,
+                'collation' => 'utf8mb4_unicode_520_nopad_ci',
+                'encoding' => 'utf8mb4',
+                'after' => 'name',
+            ])
+            ->save();
+        $this->table('activities', [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_520_nopad_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->changeColumn('apps_id', 'integer', [
+                'null' => true,
+                'default' => null,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'id',
+            ])
+            ->save();
+    }
+}
