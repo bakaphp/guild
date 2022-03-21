13ce85608e5cebbbc3c58fe42d958e9ad71b4049
arfenis <arfen00@gmail.com>
2022-03-20 23:48:16 -0400

    Remove participants types from participants

--- src/Participants/Participants.php
+++ src/Participants/Participants.php
@@ -5,17 +5,13 @@ declare(strict_types=1);
 namespace Kanvas\Guild\Participants;
 
 use Baka\Contracts\Database\ModelInterface;
-use Kanvas\Guild\Contracts\UserInterface;
 use Kanvas\Guild\Leads\Models\Leads as ModelsLeads;
 use Kanvas\Guild\Participants\Models\Participants as ModelParticipants;
 use Kanvas\Guild\Participants\Models\Types as ParticipantsTypes;
 use Kanvas\Guild\Peoples\Models\Peoples;
-use Kanvas\Guild\Traits\Searchable as SearchableTrait;
 
 class Participants
 {
-    use SearchableTrait;
-
     /**
      * Set Model for traits.
      *
@@ -42,23 +38,5 @@ class Participants
         $newParticipant->participants_types_id = $type->getId();
 
         return $newParticipant;
-    }
-
-    /**
-     * Create a new participant type.
-     *
-     * @param UserInterface $user
-     * @param string $name
-     * @return ParticipantsTypes
-     */
-    public static function createParticipantType(UserInterface $user, string $name, int $appId = null) : ParticipantsTypes
-    {
-        $newType = new ParticipantsTypes();
-        $newType->name = $name;
-        $newType->companies_id = $user->currentCompanyId();
-        $newType->apps_id = $appId ?? 1;
-        $newType->saveOrFail();
-
-        return $newType;
     }
 }
